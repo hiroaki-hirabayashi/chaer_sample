ca2695c3376294f4c65c2424bd1750cb241d1e2e
平林 宏淳 <hh@MacBook-Pro.local>
2019-09-30 21:50:44 +0900

    first commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,50 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+
+
+
+
+// Uncomment to copy all static images under ../images to the output folder and reference
+// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
+// or the `imagePath` JavaScript helper below.
+//
+// const images = require.context('../images', true)
+// const imagePath = (name) => images(name, true)
+
+window.addEventListener('DOMContentLoaded', function(){
+	var ctx = document.getElementById("myChart").getContext('2d')
+	var myChart = new Chart(ctx,{
+		type: 'bar',
+		data: {
+			labels: gon.labels,
+			datasets: [{
+				label: 'score',
+				data: gon.scores,
+				backgroundColor: [
+				'rgba(255, 99,132, 0.2)',
+				'rgba(54, 162, 235, 0.2)',
+				'rgba(255, 206, 86, 0.2)',
+				'rgba(75, 192, 192, 0.2)',
+				'rgba(153, 102, 255, 0.2)',
+				'rgba(255, 159, 64, 0.2)'
+				],
+				borderColor: [
+					'rgba(255, 99, 132, 1)',
+					'rgba(54, 162, 235, 1)',
+					'rgba(255, 206, 86, 1)',
+					'rgba(75, 192, 192, 1)',
+					'rgba(153, 102, 255, 1)',
+					'rgba(255, 159, 64, 1)'
+				],
+				borderWidth: 1
+			}]
+		}
+	});
+});
